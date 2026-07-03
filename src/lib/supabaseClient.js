914c507975107975ed72ad75107884bf77c86a92
Saroj Sen <sarojsen4952@gmail.com>
2026-07-04 01:30:29 +0530

    first commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://phekjssphmxcklqljqtt0.supabase.co';
+const SUPABASE_URL = 'https://phekjsphmxcklqljqttt.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoZWtqc3BobXhja2xxbGpxdHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMDEwMzEsImV4cCI6MjA5ODY3NzAzMX0.lz7Pc11aTs-RPtJp2jEudeYIRf2K8m9OFL5gr7nQ_Yw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
